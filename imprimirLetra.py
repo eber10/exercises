2ca424336a5c76786591f8bb6423b47d2ba75001
eber <eber.penaloza10@gmail.com>
2025-01-12 17:08:27 -0500

    agregar ejercicio para imprimir la palabra python

---
+++ imprimirLetra.py
@@ -0,0 +1,13 @@
+""""imprimir la siguiente figura
+      p
+      p y
+      p y t
+      p y t h
+      p y t h o
+      p y t h o n
+"""
+palabra="python"
+x=""
+for i in palabra:
+    x+=i
+    print(x)
